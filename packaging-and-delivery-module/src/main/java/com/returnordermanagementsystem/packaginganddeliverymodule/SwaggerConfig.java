9efebb9aa002fbfda07da7a18d50b4d9eb260279
kirtishkhandar <kirtish.khandar@gmail.com>
2022-05-23 16:05:10 +0530

    enabling swagger

---
+++ packaging-and-delivery-module/src/main/java/com/returnordermanagementsystem/packaginganddeliverymodule/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.returnordermanagementsystem.packaginganddeliverymodule;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+	
+	  @Bean
+	  public GroupedOpenApi publicApi() {
+	      return GroupedOpenApi.builder()
+	              .group("springshop-public")
+	              .packagesToScan("com.returnordermanagementsystem.packaginganddeliverymodule")
+	              .pathsToMatch("/**")
+	              .build();
+	  }
+
+}
