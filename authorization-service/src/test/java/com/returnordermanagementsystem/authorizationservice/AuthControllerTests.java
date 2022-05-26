793d7efd920e80bda96b91747cb16cec23b51a2e
kirtishkhandar <kirtish.khandar@gmail.com>
2022-05-26 22:18:52 +0530

    Junit tests

---
+++ authorization-service/src/test/java/com/returnordermanagementsystem/authorizationservice/AuthControllerTests.java
@@ -0,0 +1,82 @@
+package com.returnordermanagementsystem.authorizationservice;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.jayway.jsonpath.JsonPath;
+import com.returnordermanagementsystem.authorizationservice.payload.request.LoginRequest;
+import com.returnordermanagementsystem.authorizationservice.payload.request.SignupRequest;
+
+
+public class AuthControllerTests extends AuthorizationServiceApplicationTests {
+
+	   @Override
+	   @Before
+	   public void setUp() {
+	      super.setUp();
+	   }
+	   
+	   @Test
+	   @Order(1)
+	   public void signupTest() throws Exception {
+		  mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	      String uri = "/auth/signup";
+	      SignupRequest signupRequest = new SignupRequest();
+	      signupRequest.setUsername("user1");
+	      signupRequest.setRole(new HashSet<>(Arrays.asList("User")));
+	      signupRequest.setEmail("user1@email.com");
+	      signupRequest.setPassword("password");
+	      String inputJson = super.mapToJson(signupRequest);
+	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
+	         .contentType(MediaType.APPLICATION_JSON_VALUE)
+	         .content(inputJson)).andReturn();
+	      
+	      int status = mvcResult.getResponse().getStatus();
+	      assertEquals(200, status);
+	      String content = JsonPath.read(mvcResult.getResponse().getContentAsString(), "$.message");
+	      assertEquals("User registered successfully!", content);
+
+	   }
+	   
+		
+		  @Test
+		  @Order(2) 
+		  public void loginTest() throws Exception { 
+			  mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+			  
+			  String uri1 = "/auth/signup";
+		      SignupRequest signupRequest = new SignupRequest();
+		      signupRequest.setUsername("user2");
+		      signupRequest.setRole(new HashSet<>(Arrays.asList("User")));
+		      signupRequest.setEmail("user2@email.com");
+		      signupRequest.setPassword("password");
+		      String inputJson1 = super.mapToJson(signupRequest);
+		      mvc.perform(MockMvcRequestBuilders.post(uri1)
+		         .contentType(MediaType.APPLICATION_JSON_VALUE)
+		         .content(inputJson1)).andReturn();
+		      
+			  String uri = "/auth/signin"; 
+			  LoginRequest loginRequest = new LoginRequest();
+			  loginRequest.setUsername("user2"); 
+			  loginRequest.setPassword("password");
+			  String inputJson = super.mapToJson(loginRequest);
+			  System.out.println(inputJson); 
+			  MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
+							  .contentType(MediaType.APPLICATION_JSON_VALUE)
+							  .content(inputJson)).andReturn();
+		  
+			  int status = mvcResult.getResponse().getStatus(); 
+			  assertEquals(200, status);
+		  }
+		 
+}
--- packaging-and-delivery-module/src/test/java/com/returnordermanagementsystem/packaginganddeliverymodule/PackagingAndDeliveryModuleApplicationTests.java
+++ packaging-and-delivery-module/src/test/java/com/returnordermanagementsystem/packaginganddeliverymodule/PackagingAndDeliveryModuleApplicationTests.java
@@ -1,13 +1,25 @@
 package com.returnordermanagementsystem.packaginganddeliverymodule;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PackagingAndDeliveryModuleApplicationTests {
-
+	
+	@Autowired
+	PackagingAndDeliveryService service;
+	
 	@Test
-	void contextLoads() {
+	public void testIntegral() {
+		assertEquals(3500, service.getCharge("Integral", 10));
+	}
+	
+	@Test
+	public void testAccessory() {
+		assertEquals(2000, service.getCharge("Accessory", 10));
 	}
 
 }
